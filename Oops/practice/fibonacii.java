711553bee82ee53846ef1f9645f122ae13663772
Shubham Kumar <146741407+shubham914201@users.noreply.github.com>
2023-10-02 20:34:39 +0530

    upload fibonacci

--- Oops/practice/fibonacii.java
+++ Oops/practice/fibonacii.java
@@ -1,6 +1,21 @@
 package practice;
 public class fibonacii{
-	
+    public  static void fibo(int a,int b,int n){
+        if(n==0){
+            return ;
+        }
+        int c=a+b;
+        System.out.println(c);
+        fibo(b, c, n-1);
+    }
+	public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        int n=8;
+        System.out.println(a);
+        System.out.println(b);
+        fibo(a, b, n-2);
+    }
 	
 }
 //top 5 Ques from every topic // arrays & string
